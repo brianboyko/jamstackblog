a22d2b477f74badb0a0df24d1c6861bbba8b4aa4
brianboyko <brian.boyko@gmail.com>
2019-11-28 11:28:15 -0500

    Process.env

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 const withCSS = require("@zeit/next-css");
-
+require("dotenv").config();
 module.exports = withCSS({ cssModules: true });
+module.exports.env = {
+  // Reference a variable that was defined in the .env file and make it available at Build Time
+  API_URL: process.env.API_URL || undefined,
+  API_KEY: process.env.API_KEY || undefined
+};
